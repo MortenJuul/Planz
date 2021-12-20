d318169cc36a4f22c3ab61ae0c1d0a0342d163a6
MortenJuul <54556492+MortenJuul@users.noreply.github.com>
2021-12-19 23:21:01 -0800

    fix

--- server.js
+++ server.js
@@ -87,15 +87,15 @@ app.use(function (req, res, next) {
 })
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '..', 'frontend/build')));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 // Routes
 // app.use('/', require('./app/routes/index'))
 app.use('/auth', require('./app/routes/auth'))
 app.use('/task', require('./app/routes/task'))
 // require("./app/routes/auth.router.js")(app);
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'frontend/build/index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend/build/index.html'));
 });
 
 
